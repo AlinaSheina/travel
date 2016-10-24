3da714a8f61b53f10d79021fb95e0870ce5ed15d
belskiyroman <belskiyroman@gmail.com>
2016-10-25 00:31:34 +0300

    dist

--- docs/js/interface.js
+++ docs/js/interface.js
@@ -3,5 +3,10 @@ $(function(){
 		var thisParent = $(this).parent();
 		thisParent.find('.js-open-content').toggleClass('jshide');
 		thisParent.find('.arrow-down').toggleClass('arrow-up');
-	})
+	});
+
+	$('.full-text-btn').click(function() {
+		var thisClosest = $(this).closest('.expert-overview');
+		thisClosest.find('.full-text-block').toggleClass('full-height');
+	});
 });
