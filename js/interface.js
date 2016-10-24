f4ae7680ebe8f16432d8345a1f3711574bac8c5b
belskiyroman <belskiyroman@gmail.com>
2016-10-25 00:30:29 +0300

    global changes

--- js/interface.js
+++ js/interface.js
@@ -3,5 +3,10 @@ $(function(){
 		var thisParent = $(this).parent();
 		thisParent.find('.js-open-content').toggleClass('jshide');
 		thisParent.find('.arrow-down').toggleClass('arrow-up');
-	})
+	});
+
+	$('.full-text').click(function() {
+		var thisClosest = $(this).closest('.expert-overview');
+		thisClosest.toggleClass('full-height');
+	});
 });
