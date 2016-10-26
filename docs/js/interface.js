b1a1ec881a230a8268898f3036a327ad2502bad2
Alina <alina.sheina@gmail.com>
2016-10-26 05:19:03 -0400

    calendar add

--- docs/js/interface.js
+++ docs/js/interface.js
@@ -14,4 +14,33 @@ $(function(){
 		var sidebar = $(this).closest('.sidebar');
 		sidebar.toggleClass('sidebar-hide');
 	});
+
+	$('.calendar-item').click(function() {
+		$('.calendar-item').removeClass('active-item');
+		$(this).addClass('active-item');
+	});
+
+	var calendarItemWidth = $('.calendar-item').outerWidth();
+	var calendarItemNum = $('.calendar-item').length;
+	var calendarOverview = $('.calendar-overview');
+	var calendarViewport = $('.calendar-viewport');
+	calendarOverview.width(calendarItemWidth * calendarItemNum);
+
+	var marginLeft = 0;
+	$('.js-left').click(function() {
+		if (marginLeft < 0) {
+			marginLeft = marginLeft + calendarItemWidth;
+			calendarOverview.css({
+				'margin-left': + marginLeft + 'px'
+			});
+		};
+	});
+	$('.js-right').click(function() {
+		if ( marginLeft <= 0 && marginLeft > -(calendarOverview.width() - calendarViewport.width() - calendarItemNum * 2) ) {
+			marginLeft = marginLeft - calendarItemWidth;
+			calendarOverview.css({
+				'margin-left': + marginLeft + 'px'
+			});
+		};
+	});
 });
